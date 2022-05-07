878debcfc6ea77aaeb9b09522eb0507ef308ddb1
Mathias Notaert <mathnota@student.arteveldehs.be>
2022-05-07 17:07:13 +0200

    initial commit

---
+++ src/components/pokemon-overview/PokemonOverview.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="row pokemon-overview">
+    <div class="row justify-content-md-center mt-4" >
+        <p class="col-md-3 text-center">Pokémon found: {{ pokemonsFound.length }} / 151</p>
+        <div class="col-md-3 text-center">Time left: <span id="time">15:00</span></div>
+    </div>
+    <pokemon-card v-for="(pokemon, index) in pokemons" 
+    :key="pokemon.name" 
+    :name="pokemon.name" 
+    :url="pokemon.url" 
+    :number="index + 1"
+    :pokemons-found="pokemonsFound" />
+  </div>
+</template>
+
+<script>
+
+import PokemonCard from './PokemonCard.vue';
+
+export default {
+  components: { PokemonCard },
+name: 'PokemonOverview',
+data() {
+    return {
+      gameTime: ""
+    }
+  },
+props: {
+    pokemons: {
+      type: Array
+    },
+    pokemonsFound: {
+      type: Array
+    },
+    gameStarted: {
+      type: Boolean
+    },
+  },
+  watch: {
+    gameStarted: function () {
+      const duration = 60 * 15;
+      const display = document.querySelector('#time');
+      let timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        this.gameTime = minutes + ":" + seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+            this.$emit('stop-game');
+        }
+    }, 1000);
+    }
+  }
+}
+</script>
---
+++ src/components/pokemon-overview/insertPokemon.vue
@@ -0,0 +1,52 @@
+<template>
+<form class="form-inline mt-1">
+        <div class="form-group pokemon-form-group">
+        <input name="pokemon" class="pokemon-form" v-model="pokemonInput" autocomplete="off">
+        <button @click="checkPokemon" class="btn btn-outline-light pokemon-btn" >Check</button>
+        </div>
+</form>
+       
+</template>
+
+<script>
+export default {
+name: 'InsertPokemon',
+  data() {
+    return {
+      pokemonInput: "",
+    }
+  },
+  methods: {
+      checkPokemon(e) {
+          e.preventDefault();
+          this.$emit('check-pokemon', this.pokemonInput);
+          this.pokemonInput = "";
+      }
+  }
+}
+</script>
+
+<style scoped>
+.pokemon-form-group {
+  display: flex;
+  justify-content: center;
+}
+.pokemon-form {
+  background-color: #033860;
+  color: white;
+  border: 1px solid white;
+  padding: 5px;
+  margin-right: 10px;
+}
+
+.pokemon-form:focus {
+  outline: none;
+}
+
+.pokemon-btn {
+  background-color: #033860;
+  color: white;
+  border: 1px solid white;
+  border-radius: 0;
+}
+</style>
